ca6b678380ab483ed58871b3fadb9af72705fb15
Guilherme Silva <guilherme.1995lemes@gmail.com>
2023-12-14 10:33:29 -0300

    process image done

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module imageProcessing
 
 go 1.21.5
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	golang.org/x/image v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"image"
 	"io/fs"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
+
+	"github.com/disintegration/imaging"
 )
 
-type Result struct{}
+type Result struct {
+	srcImagePath   string
+	thumbnailImage image.Image
+	err            error
+}
 
 func getContentType(path string) (string, error) {
 
@@ -36,6 +44,7 @@ func walkPath(ctx context.Context, root string) (<-chan string, <-chan error) {
 	errorCh := make(chan error, 1)
 
 	go func() {
+		defer close(pathCh)
 		errorCh <- filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 			if err != nil {
 				return err
@@ -69,6 +78,43 @@ func walkPath(ctx context.Context, root string) (<-chan string, <-chan error) {
 
 func processImage(ctx context.Context, pathCh <-chan string) <-chan Result {
 	results := make(chan Result)
+
+	thumbnail := func() {
+		for path := range pathCh {
+			srcImage, err := imaging.Open(path)
+			if err != nil {
+				select {
+				case results <- Result{srcImagePath: path, thumbnailImage: nil, err: err}:
+				case <-ctx.Done():
+				}
+			}
+
+			image := imaging.Thumbnail(srcImage, 100, 100, imaging.Lanczos)
+
+			select {
+			case results <- Result{srcImagePath: path, thumbnailImage: image, err: nil}:
+			case <-ctx.Done():
+			}
+		}
+	}
+
+	//fan out
+	var wg sync.WaitGroup
+	maxPool := 5
+	wg.Add(maxPool)
+
+	for i := 0; i < maxPool; i++ {
+		go func() {
+			thumbnail()
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
 	return results
 }
 
