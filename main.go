2c4551c876d524572a8cdd6ffee1ef69a9582cf4
Guilherme Silva <guilherme.1995lemes@gmail.com>
2023-12-14 10:04:43 -0300

    walkPath function done

--- main.go
+++ main.go
@@ -1,12 +1,68 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
 type Result struct{}
 
+func getContentType(path string) (string, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 512)
+
+	_, err = file.Read(buffer)
+
+	if err != nil {
+		return "", err
+	}
+
+	return http.DetectContentType(buffer), nil
+}
+
 func walkPath(ctx context.Context, root string) (<-chan string, <-chan error) {
 	pathCh := make(chan string)
-	errorCh := make(chan error)
+	errorCh := make(chan error, 1)
+
+	go func() {
+		errorCh <- filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !d.Type().IsRegular() {
+				return fmt.Errorf("%v is not a file", d)
+			}
+
+			contextType, err := getContentType(path)
+
+			if err != nil {
+				return err
+			}
+
+			if contextType != "image/jpeg" {
+				return fmt.Errorf("invalid context type, expected image/jpeg, given %s", contextType)
+			}
+
+			select {
+			case pathCh <- path:
+			case <-ctx.Done():
+			}
+
+			return nil
+		})
+	}()
 
 	return pathCh, errorCh
 }
